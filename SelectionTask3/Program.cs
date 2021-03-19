9c10395b5c03122b1a6fde8c266ef72795042b00
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-03-19 15:11:41 +1100

    Add files via upload
    selection task 3 commit

---
+++ SelectionTask3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace selectiontask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int sum;
+
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            sum = (num1+num2);
+
+            if (sum == 10)
+                Console.WriteLine("Sum of numbers is equal to 10");
+            if (sum > 10)
+                Console.WriteLine("Sum of numbers is greater than 10");
+            if (sum < 10)
+                Console.WriteLine ("Sum of numbers is lower than 10");
+
+        }
+    }
+}
