503d450e40680a04e5f268686bc1d2c976ff9862
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-03-19 15:10:52 +1100

    Add files via upload
    all selection tasks commit

---
+++ SelectionTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace selectiontask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+
+            Console.WriteLine("Please Enter Your Age:");
+            age = Console.ReadLine();
+            int ageNum;
+            ageNum = int.Parse(age);
+
+            if(ageNum > 17)
+                Console.WriteLine("Legally Adult Age.");
+            else
+            if(ageNum < 18)
+                Console.WriteLine("Legally Not Adult Age.");
+                
+            }
+
+
+        }
+    }
+
---
+++ SelectionTask2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace selectiontask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+            string gender;
+            Console.WriteLine("Please Enter Your Gender (M/F):");
+            gender = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Your Age:");
+            age = Console.ReadLine();
+            int ageNum;
+            ageNum = int.Parse(age);
+
+            if(ageNum > 17)
+                Console.WriteLine(gender + " " + "is an adult.");
+            else
+            if(ageNum < 18)
+                Console.WriteLine(gender + " " + "is not an adult.");
+                
+        }
+    }
+}
